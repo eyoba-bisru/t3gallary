f4c89e8558831cf40e23b203cffca2f0a2c208b7
Eyob <eabebe91@gmail.com>
2024-07-24 21:08:23 +0300

    sign in added

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,11 @@
 import "@/styles/globals.css";
+import {
+  ClerkProvider,
+  SignedIn,
+  SignedOut,
+  SignInButton,
+  UserButton,
+} from "@clerk/nextjs";
 
 import { GeistSans } from "geist/font/sans";
 import { type Metadata } from "next";
@@ -13,7 +20,17 @@ const TopNav = () => {
   return (
     <div className="flex items-center justify-between border-b border-black p-4">
       <h1 className="text-3xl">T3 Gallery</h1>
-      <p className="text-3xl">SignIn</p>
+
+      <SignedOut>
+        <div className="text-3xl">
+          <SignInButton />
+        </div>
+      </SignedOut>
+      <SignedIn>
+        <div className="text-3xl">
+          <UserButton />
+        </div>
+      </SignedIn>
     </div>
   );
 };
@@ -22,11 +39,13 @@ export default function RootLayout({
   children,
 }: Readonly<{ children: React.ReactNode }>) {
   return (
-    <html lang="en" className={`${GeistSans.variable}`}>
-      <body className="p-4">
-        <TopNav />
-        <div className="p-4">{children}</div>
-      </body>
-    </html>
+    <ClerkProvider>
+      <html lang="en" className={`${GeistSans.variable}`}>
+        <body className="p-4">
+          <TopNav />
+          <div className="p-4">{children}</div>
+        </body>
+      </html>
+    </ClerkProvider>
   );
 }
